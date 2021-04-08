9338e621c97fbf6e4e1011b24d4682185cb4f524
splincode <omaxphp@yandex.com>
2021-04-08 11:29:32 +0300

    fixed styles

--- src/components/signIn/SignIn.js
+++ src/components/signIn/SignIn.js
@@ -10,11 +10,11 @@ const SignIn = ({ onRouteChange }) => {
                 <legend className="f2 fw6 ph0 mh0">Sign In</legend>
                 <div className="mt3">
                     <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                    <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+                    <input className="pa2 input-reset ba b--black bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
                 </div>
                 <div className="mv3">
                     <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                    <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="current-password" name="password"  id="password" />
+                    <input className="pa2 input-reset ba b--black bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
                 </div>
                 </fieldset>
                 <div className="">
