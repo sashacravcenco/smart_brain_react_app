1ea393a69c41b86c6b4143a95332a98419961437
splincode <omaxphp@yandex.com>
2021-04-09 10:35:32 +0300

    Added cors, return user on signin

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 const database = {
     users: [
         {
@@ -31,9 +33,11 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
-    if(req.body.email === database.users[0].email && 
-        req.body.password === database.users[0].password) {
-            res.json('succes');
+        const user = 
+                database.users
+                    .filter((user) => user.email === req.body.email && user.password === req.body.password)[0];
+        if (user) {
+            res.json(user);
         } else {
             res.status(400).json('Error logging in');
         }
--- frontend/src/components/signIn/SignIn.js
+++ frontend/src/components/signIn/SignIn.js
@@ -17,21 +17,21 @@ class SignIn extends React.Component {
         this.setState({signInPassword: event.target.value})
     }
     onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
-        method: 'post',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            email: this.state.signInEmail,
-            password: this.state.signInPassword
-        })
-    }) 
-    .then(response => response.json()) 
-    .then(data => {
-        if(data === 'success') {
-            this.props.onRouteChange('home');
-        }
-    })    
-}
+        fetch('http://localhost:3000/signin', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                email: this.state.signInEmail,
+                password: this.state.signInPassword
+            })
+        }) 
+        .then(response => response.json()) 
+        .then(data => {
+            if(data.id) {
+                this.props.onRouteChange('home');
+            }
+        })    
+    }
 
     render() {
         const { onRouteChange } = this.props;
