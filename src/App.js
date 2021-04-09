46663c88aed28d3cb0962be9a7995da5cea5d7e6
splincode <omaxphp@yandex.com>
2021-04-09 15:09:19 +0300

    Fixed for deployment

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ onButtonSubmit = () => {
       this.state.input)
     .then (response => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://afternoon-bayou-38612.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -17,7 +17,7 @@ class SignIn extends React.Component {
         this.setState({signInPassword: event.target.value})
     }
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://afternoon-bayou-38612.herokuapp.com/signin', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
